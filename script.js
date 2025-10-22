2158a12c373afae8d5319ea9593671efb3854b75
Chandra Kireeti Kotturu <104622340+kireetikotturu@users.noreply.github.com>
2025-10-22 07:22:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
 
+function volume_sphere(event) {
+    //Write your code here
+	event.preventDefault();
+	let userInputR = document.getElementById("radius").value;
+	let volume = document.getElementById("volume");
+	if(isNaN(userInputR) || userInputR<0){
+		volume.value = NaN;
+		return;
+	}
+	let v = 4/3*(Math.PI)*(userInputR*3);
+	volume.value = v.toFixed(4);
+} 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
